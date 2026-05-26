e616bebc39f110275f3542438ea83add4d8ab826
Shkip26 <inshin2006@mail.ru>
2026-05-26 03:18:46 +0300

    Update bd.sql

--- bd.sql
+++ bd.sql
@@ -11,12 +11,20 @@ CREATE TABLE cars (
     created_at  TIMESTAMP     NOT NULL DEFAULT NOW()
 );
 
+ALTER TABLE cars ADD COLUMN hp integer NOT NULL DEFAULT 0;
+
 INSERT INTO cars (brand, model, year, color, vin, price, mileage, description) 
 VALUES 
     ('audi', 'rs3', 2020, 'grey', '457692991865', 3500000, 66000, 'не бита не крашена, обслужевалась каждые 5к пробега'),
     ('audi', 'rs5', 2021, 'blue', '853762965937', 8900000, 76000, 'не бита не крашена, обслужевалась каждые 5к пробега'),
     ('audi', 'rs6', 2020, 'black', '089437939223', 11000000, 11000, 'не бита не крашена, обслужевалась каждые 5к пробега'),
     ('audi', 'rs7', 2024, 'white', '38970537649', 17000000, 6000, 'не бита не крашена, обслужевалась каждые 5к пробега');
+
+
+UPDATE cars SET hp = 400 WHERE model = 'rs3' AND brand = 'audi';
+UPDATE cars SET hp = 450 WHERE model = 'rs5' AND brand = 'audi';
+UPDATE cars SET hp = 600 WHERE model = 'rs6' AND brand = 'audi';
+UPDATE cars SET hp = 600 WHERE model = 'rs7' AND brand = 'audi';
         #заполняем ячейки данными 
         
 CREATE TABLE employees (
@@ -29,6 +37,10 @@ CREATE TABLE employees (
     bonus_percent  NUMERIC(5,2)  NOT NULL DEFAULT 3.00,
     hired_at       TIMESTAMP     NOT NULL DEFAULT NOW()
 );
+ALTER TABLE employees ADD COLUMN login VARCHAR(50) UNIQUE;
+ALTER TABLE employees ADD COLUMN password_hash VARCHAR(255);
+INSERT INTO employees (first_name, last_name, email, position, base_salary, bonus_percent, login, password_hash)
+VALUES ('Admin', 'Admin', 'admin2@auto.ru', 'Менеджер', 50000, 5.00, 'admin','$2b$10$FMeMkyIEU/GEuQyyt/1DDedmsQrWqu.TrlZh0Apj81UOGzpkrqety' );
 
 CREATE TABLE clients (
     id            UUID        PRIMARY KEY DEFAULT gen_random_uuid(),
